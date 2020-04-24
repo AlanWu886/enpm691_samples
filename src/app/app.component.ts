3fb9dd6b4e34f1c9b719105cfe6f890cf921faae
AlanWu886 <mlwu860401@gmail.com>
2020-04-24 16:20:10 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit {
-  post = 'test';
-  // comment = ['comment1','comment2','comment3', '<img src="11" onerror="console.log(window.localStorage)" />','<img src="11" onerror="console.log(window.navigator.userAgent)" />'];
-  // <img src="11" onerror="javascript:document.location.href='mailto:xyz@gmail.com';"/>
-  // <img src="11" onerror="javascript:window.open(\'https://alanwu886.github.io/#!\');"/>
-  // <script>window.open('https://alanwu886.github.io/#!')</script>
-  // <img src="11" onerror="javascript:document.cookie;" />
+  post = '';
 
-  comment = ['<div ONpointerover="javascript:window.open(\'https://alanwu886.github.io/#!\');">MOVE HERE</div>', '<div onpointerover="console.log(window.localStorage);">MOVE HERE</div>'];
+  comment = ['<div onpointerover="console.log(window.localStorage);">MOVE HERE</div>',
+  '<div onpointerover="javascript:window.open(decodeURIComponent(\'https%3A%2F%2Falanwu886.github.io%2F%23!\'));">MOVE HERE</div>',
+  '<img src="11" onerror="console.log(window.navigator.userAgent)" />'];
 
   ngOnInit () {
-    // this.inject()
+    this.inject()
   }
 
   addData() {
   // var userInput = document.getElementById("comment").value;
   console.log(this.post);
-  this.comment.push(this.post);  
+  this.comment.push(this.post);
+  this.post ='';
   }
 
   inject(){
-  console.log('xss attack!')
+
   const t = document.querySelector('#t')
     this.comment.forEach(item => {
       const p = document.createElement('p')
