b642a850405333bc6fd68976ca2f7dabd5e6d58c
AlanWu886 <mlwu860401@gmail.com>
2020-04-24 16:27:32 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   post = '';
 
-  comment = ['<div onpointerover="console.log(window.localStorage);">MOVE HERE</div>',
+  comments = ['<div onpointerover="console.log(window.localStorage);">MOVE HERE</div>',
   '<div onpointerover="javascript:window.open(decodeURIComponent(\'https%3A%2F%2Falanwu886.github.io%2F%23!\'));">MOVE HERE</div>',
   '<img src="11" onerror="console.log(window.navigator.userAgent)" />'];
 
@@ -19,17 +19,17 @@ export class AppComponent implements OnInit {
   addData() {
   // var userInput = document.getElementById("comment").value;
   console.log(this.post);
-  this.comment.push(this.post);
+  this.comments.push(this.post);
   this.post ='';
   }
 
   inject(){
 
-  const t = document.querySelector('#t')
-    this.comment.forEach(item => {
-      const p = document.createElement('p')
-      p.innerHTML = item
-      t.append(p)
+  const comment_box = document.querySelector('#t')
+    this.comments.forEach(item => {
+      const post = document.createElement('post')
+      post.innerHTML = item
+      comment_box.append(post)
     })
   };
 }  
