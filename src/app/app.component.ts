8fced71d593684e960dd47c93b8d1280ecfcabe7
AlanWu886 <mlwu860401@gmail.com>
2020-05-01 11:09:43 -0400

    sanitize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   post = '';
 
-  comments = ['<div onpointerover="console.log(window.localStorage);">MOVE HERE</div>',
-  '<div onpointerover="javascript:window.open(decodeURIComponent(\'https%3A%2F%2Falanwu886.github.io%2F%23!\'));">MOVE HERE</div>',
-  '<img src="11" onerror="console.log(window.navigator.userAgent)" />'];
+  // comments = ['<div onpointerover="console.log(window.localStorage);">MOVE HERE</div>',
+  // '<div onpointerover="javascript:window.open(decodeURIComponent(\'https%3A%2F%2Falanwu886.github.io%2F%23!\'));">MOVE HERE</div>',
+  // '<img src="11" onerror="console.log(window.navigator.userAgent)" />'];
+
+  comments = [];
 
   ngOnInit () {
     this.inject()
@@ -18,19 +20,33 @@ export class AppComponent implements OnInit {
 
   addData() {
   // var userInput = document.getElementById("comment").value;
-  console.log(this.post);
-  this.comments.push(this.post);
+  // console.log(this.post);
+  
+  this.comments.push(this.sanitize(this.post));
+  console.log(this.comments);
   this.post ='';
+  this.inject();
   }
 
   inject(){
-
   const comment_box = document.querySelector('#t')
     this.comments.forEach(item => {
-      const post = document.createElement('post')
-      post.innerHTML = item
-      comment_box.append(post)
+      const c = document.createElement('c')
+
+      c.innerHTML = item + '<br>';
+      console.log(item,c)
+      comment_box.append(c)
     })
   };
+
+  sanTable = 
+    {'<':'&lt;',
+    '>':'&gt;',
+    '\"':'&quot;',
+    '\'':'&apos;'};
+
+  sanitize(text){
+    return text.replace(/["'<>]/g, m => this.sanTable[m]);
+  }
 }  
 
