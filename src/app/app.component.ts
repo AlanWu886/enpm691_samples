a256d2382e90acc70a992834b292d9ac914a126e
AlanWu886 <mlwu860401@gmail.com>
2020-05-12 22:18:09 -0400

    xss filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  post = '';
+  post = "";
 
-  // comments = ['<div onpointerover="console.log(window.localStorage);">MOVE HERE</div>',
-  // '<div onpointerover="javascript:window.open(decodeURIComponent(\'https%3A%2F%2Falanwu886.github.io%2F%23!\'));">MOVE HERE</div>',
-  // '<img src="11" onerror="console.log(window.navigator.userAgent)" />'];
+  //eaxmple 1: <img src="empty" onerror="alert('xxx')">
+  //eaxmple 2: <img src="empty" onerror="javascript:document.getElementById('title').innerHTML = null;">
+  //eaxmple 3: <img src="empty" onerror="javascript:console.log(window.navigator.userAgent);">
+  //eaxmple 4: <img src="empty" onerror="javascript:console.log(window.localStorage);">
+  //<a href="javascript:alert('XSS')">redirect to...</a>
+
+
+  // comments = ['<img src="empty" onerror="jjavascript:console.log(window.navigator.userAgent);">',
+  // '<div onpointerover="javascript:window.open(decodeURIComponent(\'https%3A%2F%2Falanwu886.github.io%2F%23!\'));">MOVE HERE</div>'];
 
   comments = [];
 
-  ngOnInit () {
-    this.inject()
+  ngOnInit() {
+    this.inject();
   }
 
   addData() {
-  // var userInput = document.getElementById("comment").value;
-  // console.log(this.post);
-  
-  this.comments.push(this.sanitize(this.post));
-  console.log(this.comments);
-  this.post ='';
-  this.inject();
+    // naked input
+    this.comments.push(this.post);
+    console.log("current comment:", this.comments);
+    this.addPost();
+    
+    // escape method
+    // this.comments.push(this.encodeHTML(this.post));
+    // console.log("current comment:", this.comments);
+    // this.addPost();
+
+    // blacklist method
+    // if (this.blackList(this.post)) {
+    //   this.post = 'illegal string!';
+    // }else {
+    //   this.comments.push(this.post);
+    //   console.log("current comment:", this.comments);
+    //   this.addPost();
+    //   this.post="";
+    // }
+
+    // whitelist method
+    // if (this.whiteList(this.post)) {   
+    //   this.comments.push(this.post);
+    //   console.log("current comment:", this.comments);
+    //   this.addPost();
+    //   this.post="";
+    // }else {
+    //   this.post = 'illegal string!';
+    // }
+
   }
 
-  inject(){
-  const comment_box = document.querySelector('#t')
+  inject() {
+    const comment_box = document.querySelector("#c_box");
     this.comments.forEach(item => {
-      const c = document.createElement('c')
+      const c = document.createElement("c");
+      c.innerHTML = item + "<br>";
+      // console.log(item, c);
+      comment_box.append(c);
+    });
+  }
 
-      c.innerHTML = item + '<br>';
-      console.log(item,c)
-      comment_box.append(c)
-    })
+  addPost()  {
+    const comment_box = document.querySelector("#c_box");
+    const po = document.createElement("po");
+    po.innerHTML = this.comments[this.comments.length-1] + "<br>";
+    comment_box.append(po);
+  }
+
+  escTable = { "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&apos;" };
+
+  blacklist = {
+    javascript: "",
+    script: ""
   };
 
-  sanTable = 
-    {'<':'&lt;',
-    '>':'&gt;',
-    '\"':'&quot;',
-    '\'':'&apos;'};
+  encodeHTML(text) {
+    return text.replace(/["'<>]/g, m => this.escTable[m]);
+  };
 
-  sanitize(text){
-    return text.replace(/["'<>]/g, m => this.sanTable[m]);
+  whiteList(text) {
+    var patt = /^(\d|\w|@)/g;   
+    return patt.test(text);
+  };
+
+  blackList(text) {
+    var patt = /(javascript|script|onpointerover|onerror)/g;
+    return patt.test(text);
   }
-}  
-
+}
